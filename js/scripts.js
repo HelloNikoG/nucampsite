6340e3063ca29f62e3f1481e5ae65addfbfa5a08
Niko Garalde <nikog.nyc@gmail.com>
2021-06-01 18:06:02 -0700

    final commit

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(function(){
+    $(".carousel").carousel({interval:2000, pause: "false"});
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else{
+           $(".carousel").carousel("cycle");
+           $("#carouselButton").children("i").removeClass("fa-play");
+           $("#carouselButton").children("i").addClass("fa-pause");
+        }
+
+    });
+});
+
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    // $("#loginButton").click(function(){
+    //         $("#loginModal").modal("show");
+    })
+
+});
+
+
+$(function(){
+    // $("#reserveButton").click(function(){
+    //     $("#reserveModal").modal("show");
+    $("#loginButton").click(function(){
+            $("#loginModal").modal("show");
+    })
+});    
+//  $(function(){
+//      $(".carousel").carousel({interval:2000, pause: "false"});
+//      $("#carouselPause").click(function(){
+//          $(".carousel").carousel("pause");
+//      });
+//      $("#carouselPlay").click(function(){
+//          $(".carousel").carousel("cylce");
+//      });
+
+
+//  });
